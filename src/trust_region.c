14efee0be4840bff6a82c935b1463be5d85da0c8
Christian B. Mendl <christian.b.mendl@gmail.com>
2023-05-30 18:06:35 +0200

    implemented riemannian_trust_region_optimize

--- src/trust_region.c
+++ src/trust_region.c
@@ -51,7 +51,7 @@ static double move_to_boundary(const double* b, const double* d, int n, double r
 	double dsq = cblas_ddot(n, d, 1, d, 1);
 	if (dsq == 0)
 	{
-		fprintf(stderr, "input vector 'd' is zero");
+		fprintf(stderr, "input vector 'd' is zero\n");
 		return 0;
 	}
 	double p = cblas_ddot(n, b, 1, d, 1) / dsq;
@@ -59,7 +59,7 @@ static double move_to_boundary(const double* b, const double* d, int n, double r
 	double t[2];
 	solve_quadratic_equation(p, q, t);
 	if (t[1] < 0) {
-		fprintf(stderr, "encountered t < 0: t = %g", t[1]);
+		fprintf(stderr, "encountered t < 0: t = %g\n", t[1]);
 	}
 	return t[1];
 }
@@ -138,4 +138,81 @@ bool truncated_cg(const double* grad, const double* hess, int n, double radius, 
 	aligned_free(d);
 	aligned_free(r);
 	return false;
+}
+
+
+//________________________________________________________________________________________________________________________
+///
+/// \brief Optimization via the Riemannian trust-region (RTR) algorithm.
+///
+/// Reference:
+///     Algorithm 10 in:
+///     P.-A. Absil, R. Mahony, Rodolphe Sepulchre
+///     Optimization Algorithms on Matrix Manifolds
+///     Princeton University Press (2008)
+///
+void riemannian_trust_region_optimize(target_func f, void* fdata, retract_func retract, void* rdata,
+	gradient_func gradfunc, void* graddata, hessian_func hessfunc, void* hessdata, const int n,
+	const double* x_init, const int m, const struct rtr_params* params, double* f_iter, double* g_iter, double* x_final)
+{
+	assert(0 <= params->rho_trust && params->rho_trust < 0.25);
+
+	double* x = aligned_alloc(MEM_DATA_ALIGN, m * sizeof(double));
+	memcpy(x, x_init, m * sizeof(double));
+
+	double* x_next = aligned_alloc(MEM_DATA_ALIGN, m * sizeof(double));
+
+	double radius = params->radius_init;
+	if (params->gfunc != NULL) {
+		g_iter[0] = params->gfunc(x, params->gdata);
+	}
+
+	double* grad = aligned_alloc(MEM_DATA_ALIGN, n * sizeof(double));
+	double* hess = aligned_alloc(MEM_DATA_ALIGN, n * n * sizeof(double));
+	double* eta  = aligned_alloc(MEM_DATA_ALIGN, n * sizeof(double));
+	double* Heta = aligned_alloc(MEM_DATA_ALIGN, n * sizeof(double));
+
+	for (int k = 0; k < params->niter; k++)
+	{
+		gradfunc(x, graddata, grad);
+		hessfunc(x, hessdata, hess);
+		bool on_boundary = truncated_cg(grad, hess, n, radius, &params->tcg_params, eta);
+		retract(x, eta, rdata, x_next);
+
+		double fx = f(x, fdata);
+		f_iter[k] = fx;
+
+		// Eq. (7.7)
+		// Heta = hess @ eta
+		cblas_dgemv(CblasRowMajor, CblasNoTrans, n, n, 1.0, hess, n, eta, 1, 0, Heta, 1);
+		double rho = (f(x_next, fdata) - fx) / (cblas_ddot(n, grad, 1, eta, 1) + 0.5 * cblas_ddot(n, eta, 1, Heta, 1));
+		if (rho < 0.25) {
+			// reduce radius
+			radius *= 0.25;
+		}
+		else if (rho > 0.75 && on_boundary) {
+			// enlarge radius
+			radius = fmin(2 * radius, params->maxradius);
+		}
+
+		if (rho > params->rho_trust) {
+			// swap pointers (instead of copying data from x_next to x)
+			double* y = x;
+			x = x_next;
+			x_next = y;
+		}
+
+		if (params->gfunc != NULL) {
+			g_iter[k + 1] = params->gfunc(x, params->gdata);
+		}
+	}
+
+	memcpy(x_final, x, m * sizeof(double));
+
+	aligned_free(Heta);
+	aligned_free(eta);
+	aligned_free(hess);
+	aligned_free(grad);
+	aligned_free(x_next);
+	aligned_free(x);
 }
--- src/trust_region.h
+++ src/trust_region.h
@@ -28,3 +28,48 @@ static inline void set_truncated_cg_default_params(int n, struct truncated_cg_pa
 
 
 bool truncated_cg(const double* grad, const double* hess, int n, double radius, const struct truncated_cg_params* params, double* z);
+
+
+typedef double (*target_func)(const double* restrict x, void* fdata);
+
+typedef void (*retract_func)(const double* restrict x, const double* restrict eta, void* rdata, double* restrict xs);
+
+typedef void (*gradient_func)(const double* restrict x, void* gdata, double* restrict grad);
+typedef void  (*hessian_func)(const double* restrict x, void* hdata, double* restrict hess);
+
+
+//________________________________________________________________________________________________________________________
+///
+/// \brief Parameters for the Riemannian trust-region (RTR) algorithm.
+///
+struct rtr_params
+{
+	struct truncated_cg_params tcg_params;
+	double rho_trust;
+	double radius_init;
+	double maxradius;
+	int niter;
+	target_func gfunc;
+	void* gdata;
+};
+
+
+//________________________________________________________________________________________________________________________
+///
+/// \brief Set default parameters for the Riemannian trust-region (RTR) algorithm.
+///
+static inline void set_rtr_default_params(int n, struct rtr_params* params)
+{
+	set_truncated_cg_default_params(n, &params->tcg_params);
+	params->rho_trust   = 0.125;
+	params->radius_init = 0.01;
+	params->maxradius   = 0.1;
+	params->niter       = 20;
+	params->gfunc       = NULL;
+	params->gdata       = NULL;
+}
+
+
+void riemannian_trust_region_optimize(target_func f, void* fdata, retract_func retract, void* rdata,
+	gradient_func gradfunc, void* graddata, hessian_func hessfunc, void* hessdata, const int n,
+	const double* x_init, const int m, const struct rtr_params* params, double* f_iter, double* g_iter, double* x_final);
